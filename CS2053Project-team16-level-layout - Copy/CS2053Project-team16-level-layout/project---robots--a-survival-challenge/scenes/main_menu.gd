e215a7852266b02aa5600900941199a8280e13e0
SarahGisch <sarah.gischer@unb.ca>
2025-03-23 20:16:53 -0300

    added levels using tileset
    added tileset to levels

---
+++ CS2053Project-team16-level-layout - Copy/CS2053Project-team16-level-layout/project---robots--a-survival-challenge/scenes/main_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+const FIRST_LEVEL: String = "res://scenes/levels/level_1.tscn"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$MarginContainer/VBoxContainer/PlayButton.connect("pressed", _on_play_pressed)
+	$MarginContainer/VBoxContainer/QuitButton.connect("pressed", _on_quit_pressed)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file(FIRST_LEVEL)
+
+func _on_quit_pressed():
+	get_tree().quit()
